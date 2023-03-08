50f30ee561b9377ff869cdc14a0482cf7776a3eb
radnaan <adnaansoft@gmail.com>
2023-03-08 01:01:09 +0000

    scriptgen

--- generator/script.py
+++ generator/script.py
@@ -1,22 +1,64 @@
-# Generate a new title and script using GPT-3
-prompt = 'Generate a video title and script based on the trending topics on Reddit and YouTube.'
+import praw
+import openai
 
-model = 'text-davinci-002'
-temperature = 0.5
-max_tokens = 1024
+# Set up the OpenAI API client
+openai.api_key = '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'
 
-response = openai.Completion.create(
-    engine=model,
-    prompt=prompt,
-    temperature=temperature,
-    max_tokens=max_tokens,
-    n=1,
-    stop=None,
-    timeout=60,
+# Set up the Reddit API client
+reddit = praw.Reddit(
+    client_id='gXXLFD9GbXhvZ-gY9TBfrg',
+    client_secret='3rB9FLDEkRJ8OS7occwyjfdp01OjJw',
+    user_agent='Josuke',
 )
 
-title = response.choices[0].text.strip()
-script = response.choices[0].text.strip()
+def get_relevant_subreddits(topic):
+    """Find relevant subreddits for a given topic"""
+    search_results = reddit.subreddit('all').search(topic, limit=5)
+    subreddits = [result.subreddit.display_name for result in search_results]
+    return subreddits
 
+def get_top_posts(subreddits):
+    """Get the top posts from relevant subreddits"""
+    posts = []
+    for subreddit in subreddits:
+        subreddit = reddit.subreddit(subreddit)
+        for post in subreddit.top(limit=5):
+            if not post.stickied:
+                post_text = post.title + post.selftext
+                if len(post_text) > 4000:
+                    post_text = post_text[:4000]
+                posts.append(post_text)
+    return posts
+
+def generate_title_and_script(posts):
+    """Generate a new title and script using GPT-3"""
+    prompt = ('Generate video title, script based on these top posts from subreddits.'+ '\n'.join(posts))[:300]
+
+    model = 'text-davinci-002'
+    temperature = 0.5
+
+    response = openai.Completion.create(
+        model=model,
+        prompt=prompt,
+        temperature=1,
+        n=1,
+        max_tokens=4000-len(prompt),
+        stop=None,
+        timeout=60
+    )
+
+    print(response)
+    title = response.choices[0].text.strip()
+    script = response.choices[0].text.strip()
+
+    return title, script
+
+# Example usage
+topic = 'horror stories'
+subreddits = get_relevant_subreddits(topic)
+posts = get_top_posts(subreddits)
+title, script = generate_title_and_script(posts)
+
+print('Relevant subreddits:', subreddits)
 print('Generated title:', title)
 print('Generated script:', script)
